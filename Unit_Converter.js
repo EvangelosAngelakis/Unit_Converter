1a78ab4d57ac0dd40dc7fc31682fdc48c59fb2d6
Evangelos Angelakis <167527848+EvangelosAngelakis@users.noreply.github.com>
2025-01-26 11:20:44 +0000

    HTML additions & JS build up
    Added values to HTML and switch operations to JS to convert the values in HTML.

--- Unit_Converter.js
+++ Unit_Converter.js
@@ -12,3 +12,80 @@ executeButton.addEventListener('click', () => {
         resultDisplay.textContent = "Please enter a valid number"
         return;
     }
+
+    let result;
+
+    switch (operation) {
+         case "MetersToFeet":
+             result = num1 * 3.281;
+             break;
+         case "MetersToInches":
+             result = num1 * 39.37;
+             break;
+         case "FeetToMeters":
+             result = num1 / 3.281;
+             break;
+         case "FeetToInches":
+             result = num1 * 12;
+             break;
+         case "InchesToMeters":
+             result = num1 / 39.3701;
+             break;
+         case "InchesToFeet":
+             result = num1 / 12;
+             break;
+         case "NauticalMilesPerHourToKilometersPerHour":
+             result = num1 * 1.852;
+             break;
+         case "NauticalMilesPerHourToKnots":
+             result = num1 * 1; 
+             break;
+         case "StatuteMilesPerHourToKnots":
+             result = num1 * 1.151; 
+             break;
+         case "StatuteMilesPerHourToKilometersPerHour":
+             result = num1 * 1.60934; 
+             break;             
+         case "KilometersPerHourToNauticalMilesPerHour":
+             result = num1 / 0.53995680345572;
+             break;
+         case "KilometersPerHourToKnots":
+             result = num1 / 1.852; 
+             break;
+         case "KnotsToNauticalMilesPerHour":
+             result = num1 * 1; 
+             break;
+         case "KnotsToKilometersPerHour":
+             result = num1 * 1.852;
+             break;
+         case "KnotsToStatuteMilesPerHour":
+             result = num1 * 1.15;
+             break;
+         case "LitersToKilograms":
+             result = num1 * 1; 
+             break;
+         case "KilogramsToLiters":
+             result = num1 * 1; 
+             break;
+         case "MillilitersToGrams":
+             result = num1 * 1; 
+             break;
+         case "GramsToMilliliters":
+             result = num1 * 1; 
+             break;
+         case "MiliLitersToKilograms":
+             result = num1 / 1000; 
+             break;
+         case "KilogramsToMiliLiters":
+             result = num1 * 1000; 
+             break;
+         case "MillilitersToGrams":
+             result = num1 * 1; 
+             break;
+         case "GramsToMilliliters":
+             result = num1 * 1; 
+             break;
+    }
+
+
+})
